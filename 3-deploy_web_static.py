29004df36378b38f6a8aec15db8177219969f357
IVashty <vashtylavilavi50@gmail.com>
2023-01-24 00:18:22 +0300

    3-deploy_web_static.py

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+now deploy the archive
+"""
+from fabric.api import *
+import os.path
+from fabric.operations import run, put, env
+
+
+env.hosts = ["52.55.249.213", "54.157.32.137"]
+
+
+def deploy():
+    """
+    deploys the archive to the web servers
+    """
+    archive_path = do_pack()
+    if not archive_path:
+        return False
+    return do_deploy(archive_path)
