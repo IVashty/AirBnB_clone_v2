d1bb4a3dd22ca67cdcc2ee6e9405ecd2501f6432
IVashty <vashtylavilavi50@gmail.com>
2023-01-24 17:40:37 +0300

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -30,4 +30,5 @@ echo "
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 #missing step-not sure
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
