754ac7d605d7f78670d46458df2f0fe39ababda9
IVashty <vashtylavilavi50@gmail.com>
2023-01-24 18:04:16 +0300

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,17 +6,19 @@ of your AirBnB Clone repo, using the function do_pack.
 """
 from fabric.api import *
 from datetime import datetime
+import os
 
 
 def do_pack():
     """
-    archive created must be web_static_<year><month><day><hour><minute><second>.tgz
+    archive created must be
+    web_static_<year><month><day><hour><minute><second>.tgz
     """
     date = datetime.now().strftime("%Y%m%d%H%M%S")
-    try:
+    if isdir("versions") is False:
         local("mkdir -p versions")
         filename = "web_static_" + date + ".tgz"
         local("tar -cvzf versions/" + filename + " web_static")
         return "versions/" + filename
-    except:
+    else:
         return None
